be60c74a54d05a87cb7f4abdec269e2d89c2a873
libardo.salamanca <libardo.salamanca@thomasgreg.com>
2023-09-23 09:03:15 -0500

    Initial commit

---
+++ doc/readme.md
@@ -0,0 +1,21 @@
+# Capacitación Universidad Agustiniana
+# *Python 3*
+
+> 23 Septiembre 2023
+
+[Colab](https://colab.research.google.com/?hl=es)
+
+Variable -> Espacio en memoria temporal RAM.
+
+> Tipos de Datos 
+> - Booleanos
+> - Texto
+> - Numeros Enteros
+> - Numeros Complejos
+> - Numeros Reales
+> 
+> Estructuras de Datos
+> - Arrays
+> - Dictionary
+> - Set
+> - Tuplas
---
+++ main.py
@@ -0,0 +1,37 @@
+# Palabras Reservadas
+print(help('keywords'))
+
+# Variables
+Saludo = "Hola"
+# Variables de Scope protegido
+_saludo = "Hola"
+
+# VAriables de Scope Privado
+__saludo = 'Hola'
+
+# snakecase
+saludo_Uniagustiniana = 'hola'
+
+# camelcase
+saludoUniagustiniana = 'hola'
+
+# Constante
+SALUDO = 'hola'
+
+# Caracteres de Escape
+
+saludo_largo = 'Hola buenos dias,\nEspero se encuentren muy bien'
+print(saludo_largo)
+
+print("'Hola Mundo'")
+
+# Booleanos
+
+is_alive = True
+print(is_alive)
+is_alive = False
+print(f"Variables de negacion {is_alive} al cambiar los valores correspondientes")
+
+# Negacion
+is_alive = not True
+print(is_alive)
