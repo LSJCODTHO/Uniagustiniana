6e788f758c7aa51ba8efabe1e560c53eaddb1c3e
libardo.salamanca <libardo.salamanca@thomasgreg.com>
2023-09-23 10:27:22 -0500

    Actualizacion de Methodo Input

--- 23092023/02_input.py
+++ 23092023/02_input.py
@@ -1,7 +1,14 @@
 # Entrada por consola
 
 numero_identificacion = int(input("Escriba su numero de identificacion : "))
-print(numero_identificacion)
 
 nombre_empleado = str(input("Escriba su Nombre Completo : "))
-print(nombre_empleado)
+
+# Entradas dinamicas
+
+edad = int(input(f"Hola {nombre_empleado} identificion No {numero_identificacion}, ingrese su edad por favor : "))
+
+if edad >= 18:
+    print(f"{nombre_empleado} usted es Mayor de edad")
+else:
+    print(f"{nombre_empleado} Usted es Menor de Edad")
